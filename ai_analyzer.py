782cdc644b4cd0cf965c02ec9cdfb2cb575726ba
Keerthanabs627 <keerthanabs627@gmail.com>
2026-08-12 23:36:28 +0530

    Move API key to env file

--- ai_analyzer.py
+++ ai_analyzer.py
@@ -1,9 +1,12 @@
 from google import genai
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 client = genai.Client(
     api_key=os.getenv("GEMINI_API_KEY")
 )
-
 def analyze_resume(resume_text):
 
     prompt = f"""
